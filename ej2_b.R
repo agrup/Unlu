76004231cba8ac57596ad649910f3376e7dcd9a6
agrup <agustinrodriguez206@gmail.com>
2017-10-12 14:54:17 -0300

    agregada funcion para anchos de bin

--- ej2_b.R
+++ ej2_b.R
@@ -5,20 +5,27 @@ BindeqAncho<-function(Datos,ab){
   max=max(datatime)
   min=min(datatime)
   rango = max-min
+  j=1
   #rango=length(datatime)
   #nl=round(rango/ab)
+  c=0
   cb = round(rango/ab)
   aux<-0
   a=min
   b=min+ab
   for(i in 1 :cb){
-    aux[i]<-length(datatime)
+    while ( (datatime[j] <= b) & (j <= length(datatime) )){
+    c=c+1
+    j=j+1
+    }
+    aux[i]<-c
+    c=0
     a=b
     b=b+ab
   }
+
   return(aux)
 }
-length(dataset$Tiempo_Traslado)
-barplot(BindeqFrec(dataset$Tiempo_Traslado,11))
 
-plot(dataset$Tiempo_Traslado)
+barplot(BindeqAncho(dataset$Tiempo_Traslado,50))
+
